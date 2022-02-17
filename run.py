d05080b26b64a1bf5d621ae245bfdd7025b5cef0
gerardambe <gerardambe@yahoo.com>
2022-02-17 21:31:35 +0000

    added send email

--- run.py
+++ run.py
@@ -18,8 +18,8 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = os.environ.get("nilssonstadtjanst@gmail.com")
-app.config['MAIL_PASSWORD'] = s.environ.get('obtmimgynaxizidz')
+app.config['MAIL_USER'] = os.environ.get("EMAIL_USER")
+app.config['MAIL_PASS'] = s.environ.get('EMAIL_PASS')
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 mail = Mail(app)
